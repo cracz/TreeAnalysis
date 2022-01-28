c62c9d3aa0c5a619c69309012d1ff34344080e46
ErikMLoyd <eloyd001@ucr.edu>
2022-01-28 15:34:21 -0500

    Missig braces cause this to not compile

--- StRoot/IClasses/IEvent.h
+++ StRoot/IClasses/IEvent.h
@@ -1,4 +1,5 @@
-// author: Mike Lisa 14 feb 2013 // edited for just lambdas Dec 5 by Isaac Upsal
+// author: Erik Loyd
+// Based off of code by Mike Lisa
 
 #ifndef IEVENT_H
 #define IEVENT_H
@@ -18,23 +19,32 @@ class IEvent : public TObject {
 
  protected:
 
+/*
   Int_t mRunNumber;
   Int_t mChronologicalRunId;
   Int_t mEventID;  // within run
-  Int_t mRefMult;
-  Int_t mCentralityID9;		//centrality ID number from StRefMultCorr RefMult9
-  Int_t mCentralityID16;	//centrality ID number from StRefMultCorr RefMult16
-  TVector3 mPrimaryVertex;
   Float_t mBfield;
+  TVector3 mPrimaryVertex;
+  Int_t mRefMult;
+  Int_t mCentrality;
   Float_t mVpdVz;
-  double mqCenter[2];
   std::vector<unsigned int> mTriggers;
-  
+  Short_t             EPDid[744];
+  UShort_t            nEPDhits;
+
+  UShort_t            tree_tracknumber; //What is this?
+ 
+*/
+
+
+  Float_t EPDnMip[2][12][31]; //0 = E, 1 = W
+ 
+  Float_t mqCenter[2];  // This is the Q recentering offset for all events in this data set
   std::vector<IEventPlane> mEPParticles;
   
-  void CalcQVector(int, std::vector<double>&, std::vector<double>&);
-  std::vector<double> CalcPsi(int);
-
+  void CalcQVector(int, std::vector<float>&, std::vector<float>&);
+  std::vector<float> CalcPsi(int);
+ 
  public:
 
   IEvent();
@@ -47,39 +57,45 @@ class IEvent : public TObject {
   void AddEPParticle(IEventPlane n){mEPParticles.push_back(n);}
   void AddEPParticle(std::vector<IEventPlane> n){mEPParticles = n;}
   
-  std::vector<double> GetPsi(int);
-  std::vector<double> GetPsi(int, char, double, double);
-  double GetQx(int);
-  double GetQy(int);
+  std::vector<float> GetPsi(int);
+  std::vector<float> GetPsi(int, char, float, float);
+  Float_t GetQx(int);
+  Float_t GetQy(int);
 
+/*
   TVector3 PrimaryVertex() {return mPrimaryVertex;}
   void SetPrimaryVertex(TVector3 pv){mPrimaryVertex = pv;}
-
   Int_t GetRunNumber(){return mRunNumber;}
   Int_t GetChronologicalRunId(){return mChronologicalRunId;}
   Int_t GetEventID(){return mEventID;}
   Int_t GetRefMult(){return mRefMult;}
-  Int_t GetCentralityID9(){return mCentralityID9;}
-  Int_t GetCentralityID16(){return mCentralityID16;}
+  Int_t GetCentralityID9(){return mCentrality}
   Float_t Bfield(){return mBfield;}
   Float_t GetVpdVz(){return mVpdVz;}
-  std::vector<unsigned int> GetTriggers(){return mTriggers;}
+  
+  */
+  //std::vector<unsigned int> GetTriggers(){return mTriggers;}
   std::vector<IEventPlane> GetEPParticles(){return mEPParticles;}
 
-
+/*
   void SetRunNumber(Int_t rn){mRunNumber=rn;}
   void SetChronologicalRunId(Int_t rn){mChronologicalRunId=rn;}
   void SetEventID(Int_t en){mEventID=en;}
   void SetRefMult(Int_t rm){mRefMult = rm;}
-  void SetCentralityID9(Int_t cent){mCentralityID9=cent;}
-  void SetCentralityID16(Int_t cent){mCentralityID16=cent;}
+  void SetCentrality(Int_t cent){mCentrality=cent;}
   void SetBfield(Float_t f){mBfield=f;}
   void SetVpdVz(Float_t n){mVpdVz=n;}
   void SetTriggers( const std::vector<unsigned int> & n){mTriggers = n;}
+*/ 
+ 
   void SetEPParticles( const std::vector<IEventPlane> & n){mEPParticles = n;}
-  void SetQCenter(double, double);
+  void SetQCenter(float, float);
+  
+  IEventPLane EPDVector();
+  
+  Float_t GetEPDnMip(int ew, int pp, int tt){return EPDnMip[ew][pp - 1][tt - 1];}
 
-  IEvent GetSubEvent(char, double, double);
+  IEvent GetSubEvent(char, float, float);
 
   ClassDef(IEvent,1)  // my event
 };
